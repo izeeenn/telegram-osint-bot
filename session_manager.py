5acbaa7f4f55d123a07b848fa86d1c86a4f92352
Jan Molina Tudela <107145481+izeeenn@users.noreply.github.com>
2025-03-21 18:56:55 +0100

    Update session_manager.py

--- session_manager.py
+++ session_manager.py
@@ -1,15 +1,15 @@
 import os
 
-SESSION_FILE = "session_id.txt"
+SESSION_FILE = "session.txt"
 
 def load_session():
-    """ Cargar SESSION_ID desde un archivo o entorno """
+    """ Cargar SESSION_ID desde archivo """
     if os.path.exists(SESSION_FILE):
-        with open(SESSION_FILE, "r") as f:
-            return f.read().strip()
+        with open(SESSION_FILE, "r") as file:
+            return file.read().strip()
     return os.getenv("SESSION_ID", "")
 
 def save_session(new_session):
-    """ Guardar SESSION_ID en un archivo """
-    with open(SESSION_FILE, "w") as f:
-        f.write(new_session)
+    """ Guardar SESSION_ID en archivo """
+    with open(SESSION_FILE, "w") as file:
+        file.write(new_session)
